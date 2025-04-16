92e7e0937caf0ca3d860d08a9c191f73c9a5c65d
Harish <107391575+8555936327@users.noreply.github.com>
2025-04-17 01:36:27 +0530

    Create index.js

---
+++ app/index.js
@@ -0,0 +1,11 @@
+exports.handler = async (event) => {
+    console.log("Hello, World!");
+    console.log("Event: ", JSON.stringify(event, null, 2));
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'Hello, World!',
+        }),
+    };
+};
+
